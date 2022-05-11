7db2b7b791b84feb2de4268709c6bcab4564c6ba
Bestmart721 <steven93721@gmail.com>
2022-05-11 18:19:59 +0300

    update: jotform

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,12 +1,13 @@
 <template>
-  <iframe id="JotFormIFrame-221302827438958" 
+  <iframe 
+  id="JotFormIFrame-221302827438958" 
   ref = "iframe"
   title="Contact Form With Vanilla Theme" 
   onload="window.parent.scrollTo(0,0)" 
   allowtransparency="true" 
   allowfullscreen="true" 
-  src="https://form.jotform.com/221302827438958" 
-  frameborder="0" style=" min-width: 100%; height:1080px; border:none;" scrolling="no" 
+  src="https://form.jotform.com/221304936164957" 
+  frameborder="0" style=" min-width: 100%; height:600px; border:none;" scrolling="no" 
   > 
   </iframe>
 </template>
